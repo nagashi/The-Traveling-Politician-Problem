e388435bee6a5b38fd24b3754423121b5fd2aaa6
Charles ORiley <ceoriley@gmail.com>
2020-09-20 16:32:34 -0500

    (Not used) - function returns factoral number

--- src/permutate/mod.rs
+++ src/permutate/mod.rs
@@ -1,6 +1,6 @@
 // Return the factoral number
 // used isize to match the ability
-// of the underlying architecture
+// of the underlying architecture 
 pub fn factorial(num: isize) -> isize {
     match num {
         0 | 1 => 1,
