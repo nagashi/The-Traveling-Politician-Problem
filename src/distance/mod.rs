8d761c92df2be0d3721f3ca14fbd3a6c38ab60b4
Charles ORiley <ceoriley@gmail.com>
2020-09-20 16:22:40 -0500

    Haversine formula - convert distance to 1 decimal point

--- src/distance/mod.rs
+++ src/distance/mod.rs
@@ -10,5 +10,5 @@ pub fn haversine_dist(mut th1: f64, mut ph1: f64, mut th2: f64, ph2: f64) -> f64
     let km_to_mi = 1.609344_f64; // convert default kilometers to miles.
     let d: f64 = ((dx * dx + dy * dy + dz * dz).sqrt() / 2.0).asin() * 2.0 * r;
     let d = ((d / km_to_mi) * 10.0).round() / 10.0; // convert distance to 1 decimal point.
-    d // To be returned
+    return d // To be returned
 }
