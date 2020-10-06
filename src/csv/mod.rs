7bb7dc99e6dc4ddce32739e6fb153decad929b65
Charles ORiley <ceoriley@gmail.com>
2020-10-05 21:12:40 -0500

    Modified Location struct

--- src/csv/mod.rs
+++ src/csv/mod.rs
@@ -1,8 +1,8 @@
 use log::error;
 use std::{fs, fs::OpenOptions};
 
-pub struct Location {
-    pub path: String,
+pub struct Location<'a> {
+    pub path: &'a String,
     pub boolean: bool,
     pub cnt: usize,
 }
@@ -12,7 +12,26 @@ pub fn path_exists(path: &str) -> bool {
 }
 
 pub fn write_csv(loc: Location) -> std::fs::File {
-    match (loc.cnt, loc.boolean) {
+    match (&loc.cnt, &loc.boolean) {
+        (0, false) => {
+            // first time file writing
+            // and the file does not
+            // exists so create it
+            let file = match OpenOptions::new()
+                .write(true)
+                .create(true)
+                .append(true)
+                .open(&loc.path)
+            {
+                Ok(f) => f,
+                Err(e) => {
+                    let msg = "cypher.csv creation error";
+                    error!("{}: {}: {}: {}", msg, &loc.cnt, &loc.boolean, e);
+                    panic!("{}: {}: {}: {}", msg, &loc.cnt, &loc.boolean, e)
+                }
+            };
+            return file;
+        }
         (0, true) => {
             // if this is the first time file
             // writing and the file exists then
@@ -24,62 +43,34 @@ pub fn write_csv(loc: Location) -> std::fs::File {
             {
                 Ok(f) => f,
                 Err(e) => {
-                    let msg = "file truncate error";
-                    error!("{}: {}: {}: {}", msg, loc.cnt, loc.boolean, e);
-                    panic!("{}: {}: {}: {}", msg, loc.cnt, loc.boolean, e)
+                    let msg = "cypher.csv truncate error";
+                    error!("{}: {}: {}: {}", msg, &loc.cnt, &loc.boolean, e);
+                    panic!("{}: {}: {}: {}", msg, &loc.cnt, &loc.boolean, e)
                 }
             };
 
             // After truncating, set file variable
             // to append writing.
-            let file = match OpenOptions::new()
-                .write(true)
-                .create(true)
-                .append(true)
-                .open(&loc.path)
-            {
+            let file = match OpenOptions::new().write(true).append(true).open(&loc.path) {
                 Ok(file) => file,
                 Err(e) => {
-                    let msg = "file append error";
-                    error!("{}: {}: {}: {}", msg, loc.cnt, loc.boolean, e);
-                    panic!("{}: {}: {}: {}", msg, loc.cnt, loc.boolean, e)
+                    let msg = "cypher.csv append after truncating error";
+                    error!("{}: {}: {}: {}", msg, &loc.cnt, &loc.boolean, e);
+                    panic!("{}: {}: {}: {}", msg, &loc.cnt, &loc.boolean, e)
                 }
             };
             return file;
         }
-        (.., true) => {
-            // count > 0 and file exist (true)
-            // then continue append writing.
-            let file = match OpenOptions::new()
-                .write(true)
-                .create(true)
-                .append(true)
-                .open(loc.path)
-            {
+        (.., _) => {
+            // row > 0 so it does
+            // not matter whether file
+            // exist or not: just append writing.
+            let file = match OpenOptions::new().write(true).append(true).open(loc.path) {
                 Ok(file) => file,
                 Err(e) => {
-                    let msg = "file append error";
-                    error!("{}: {}: {}: {}", msg, loc.cnt, loc.boolean, e);
-                    panic!("{}: {}: {}: {}", msg, loc.cnt, loc.boolean, e)
-                }
-            };
-            return file;
-        }
-        (.., false) => {
-            // file doesn't exist, create
-            // & set file properties to
-            // append writing.
-            let file = match OpenOptions::new()
-                .write(true)
-                .create(true)
-                .append(true)
-                .open(loc.path)
-            {
-                Ok(file) => file,
-                Err(e) => {
-                    let msg = "file append error";
-                    error!("{}: {}: {}: {}", msg, loc.cnt, loc.boolean, e);
-                    panic!("{}: {}: {}: {}", msg, loc.cnt, loc.boolean, e)
+                    let msg = "cypher.csv row > 0 append error";
+                    error!("{}: {}: {}: {}", msg, &loc.cnt, &loc.boolean, e);
+                    panic!("{}: {}: {}: {}", msg, &loc.cnt, &loc.boolean, e)
                 }
             };
             return file;
